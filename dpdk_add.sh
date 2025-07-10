582523b7061347336b445bf1a0390bcd735f132b
Sergio Elizalde <sergio.elizaldefranco@gmail.com>
2025-07-09 23:07:19 -0400

    correcting the dpdk add

--- dpdk_add.sh
+++ dpdk_add.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # Add Mellanox DOCA pkg-config path
-sudo export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/mellanox/doca/lib/aarch64-linux-gnu/pkgconfig
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/mellanox/doca/lib/aarch64-linux-gnu/pkgconfig
 
 # Add Mellanox DOCA tools to PATH
-sudo export PATH=${PATH}:/opt/mellanox/doca/tools
+export PATH=${PATH}:/opt/mellanox/doca/tools
 
 # Add Mellanox DPDK pkg-config path
-sudo export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/mellanox/dpdk/lib/aarch64-linux-gnu/pkgconfig
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/mellanox/dpdk/lib/aarch64-linux-gnu/pkgconfig
 
 # Add Mellanox FlexIO pkg-config path
-sudo export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/mellanox/flexio/lib/pkgconfig
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/mellanox/flexio/lib/pkgconfig
 
 # Add DPDK include pkg-config path
-sudo export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/mellanox/dpdk/include/aarch64-linux-gnu/dpdk
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/mellanox/dpdk/include/aarch64-linux-gnu/dpdk
 
 sudo sh -c  "echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages"
 
