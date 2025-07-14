5849e3dfe760f68c1428d3e37dd045135a86f523
Sergio Elizalde <sergio.elizaldefranco@gmail.com>
2025-07-14 04:07:14 -0400

    adding last test bigger

--- iot_mlp/iot_multicore.c
+++ iot_mlp/iot_multicore.c
@@ -84,8 +84,8 @@
 #define MAX_CORES       RTE_MAX_LCORE
 
 /* Statically allocate pools for every possible lcore */
-static struct flow_entry flow_pools[MAX_CORES][MAX_FLOWS_PER_CORE];
-static struct rte_hash *flow_tables[MAX_CORES];
+//static struct flow_entry flow_pools[MAX_CORES][MAX_FLOWS_PER_CORE];
+//static struct rte_hash *flow_tables[MAX_CORES];
 
 struct flow_key {
     uint32_t src_ip;
