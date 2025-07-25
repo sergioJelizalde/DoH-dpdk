6c296dc3477afa99921e15ce22dfa56d2095b3d5
sergioJelizalde <sergio.elizaldefranco@gmail.com>
2025-07-25 17:27:23 -0400

    just transfer mode

--- flow_rules/flow.c
+++ flow_rules/flow.c
@@ -21,8 +21,7 @@ static volatile uint64_t core_pkt_counter[RTE_MAX_LCORE] = {0};
 static struct rte_flow *
 create_mirror_rule(uint16_t src_port, uint16_t dst_port_id, struct rte_flow_error *error) {
     struct rte_flow_attr attr = {
-    .ingress = 1,
-    .transfer = 1,  // required for port-based mirror/forward actions
+    .transfer = 1,  // required for mirroring between physical/virtual ports
 };
 
     struct rte_flow_item pattern[] = {
