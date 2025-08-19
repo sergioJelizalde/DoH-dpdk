ea8f1360fbe49e7d439272675f4af0f51314632d
sergioJelizalde <sergio.elizaldefranco@gmail.com>
2025-08-19 12:33:25 -0400

    not removing flows

--- iot_mlp/iot_multicore.c
+++ iot_mlp/iot_multicore.c
@@ -540,8 +540,8 @@ handle_packet(struct flow_key   *key,
 
         // cleanup flows
         //rte_hash_del_key(w->flow_table, key);
-        reset_entry_per_core(w, index);
-        
+        //reset_entry_per_core(w, index);
+
     }
 }
 
