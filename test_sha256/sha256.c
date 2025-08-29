d13da8707e406205e3a9e92539d4144138b3bbce
sergioJelizalde <sergio.elizaldefranco@gmail.com>
2025-08-29 11:08:11 -0400

    test

--- test_sha256/sha256.c
+++ test_sha256/sha256.c
@@ -243,9 +243,7 @@ int main(int argc, char **argv) {
 
     /* Message to hash */
     const char msg[] =
-        "771,4865-4866-4867-49195-49199-49196-49200-"
-        "52393-52392-49171-49172-156-157-47-53-"
-        "10,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-21,29-23-24,0";
+        "Sergio"
     size_t len = strlen(msg);
 
     printf("strlen(msg): %zu\n", len);
