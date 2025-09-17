c3077b37b243bded5437c8181374f106c65958d3
sergioJelizalde <sergio.elizaldefranco@gmail.com>
2025-09-17 12:16:16 -0400

    adding doh mlp for dga and doh class

---
+++ doh_mlp/feature_stats.h
@@ -0,0 +1,16 @@
+// Auto-generated feature stats for z-score normalization
+
+#pragma once
+
+#define ALIGN16 __attribute__((aligned(16)))
+#define NUM_FEATURES 8
+
+static const float FEATURE_MEAN[NUM_FEATURES] ALIGN16 = {
+    765.794800f, 156376.703125f, 0.000000f, 170.495255f, 747.174255f, 1.159746f, 0.000135f, 658.881592f
+};
+
+static const float FEATURE_STD[NUM_FEATURES] ALIGN16 = {
+    5285.872559f, 191975.500000f, 1.000000f, 95.914375f, 450.903900f, 4.248154f, 0.003809f, 5070.570312f
+};
+
+#undef ALIGN16
